22745c1ad78e69b3d85f5f1129fd5d9367ae63e4
ahithmohan <ahitmohan123@gmail.com>
2024-11-20 19:16:16 +0530

    call data

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,18 +11,67 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool isLoading = true;
+  List<Map<String, dynamic>> userList = [];
+  // Map<String, dynamic>? user;
+  void fetchUser() async {
+    final response = await http
+        .get(Uri.parse("https://jsonplaceholder.typicode.com/todos/27"));
+    if (response.statusCode == 200) {
+      final body = response.body;
+      final json = jsonDecode(body);
+      print(json);
+      setState(() {
+        // user = json;
+        userList.add(json);
+        isLoading = false;
+        print(userList);
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    // fetchUser();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.black,
-        onPressed: () {},
+        onPressed: () {
+          fetchUser();
+        },
         child: Text(
           "click",
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: Center(child: Text("data")),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Center(
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (userList.isNotEmpty) ...[
+                  Text(userList[0]['userId'].toString()),
+                  Text(userList[0]['title']),
+                  Text("Completed :  ${userList[0]['completed'].toString()}"),
+                  TextButton(
+                      onPressed: () {
+                        setState(() {
+                          userList.removeAt(0);
+                        });
+                      },
+                      child: Text("delete"))
+                ] else ...[
+                  Text("no data available")
+                ]
+              ],
+            )),
     );
   }
 }
